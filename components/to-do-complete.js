a226c2b7beafa44a0d3d5d6d0efef488ca9f131e
Ignacio1996 <>
2023-04-21 13:24:34 +0000

    Pushed from Locofy Builder

--- components/to-do-complete.js
+++ components/to-do-complete.js
@@ -4,6 +4,7 @@ const ToDoComplete = ({ todoValue }) => {
     <div className={styles.todocomplete}>
       <img className={styles.image1Icon} alt="" src="/image-1@2x.png" />
       <div className={styles.finishDesigns}>{todoValue}</div>
+      <img className={styles.vectorIcon} alt="" src="/vector.svg" />
     </div>
   );
 };
