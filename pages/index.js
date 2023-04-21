6c891cec6463bfcb8d0d9a18e318b619a7cca144
Ignacio1996 <>
2023-04-21 12:50:42 +0000

    Pushed from Locofy Builder

--- pages/index.js
+++ pages/index.js
@@ -3,41 +3,6 @@ const ToDoListDesign = () => {
   return (
     <div className={styles.toDoListDesign}>
       <div className={styles.todolistcontainer}>
-        <div className={styles.addToDo}>Add to do...</div>
-        <div className={styles.divider} />
-        <div className={styles.todopending}>
-          <img className={styles.image2Icon} alt="" src="/image-2@2x.png" />
-          <div className={styles.updateFlow}>Update flow</div>
-        </div>
-        <div className={styles.todopending1}>
-          <img className={styles.image2Icon} alt="" src="/image-2@2x.png" />
-          <div className={styles.updateFlow}>Add state management</div>
-        </div>
-        <div className={styles.todopending2}>
-          <img className={styles.image2Icon} alt="" src="/image-2@2x.png" />
-          <div className={styles.updateFlow}>Push to code</div>
-        </div>
-        <div className={styles.todopending3}>
-          <img className={styles.image2Icon} alt="" src="/image-2@2x.png" />
-          <div className={styles.exportToBuilder}>
-            Export to builder and make components
-          </div>
-        </div>
-        <div className={styles.todocomplete}>
-          <div className={styles.updateFlow}>
-            Tag with Locofy and make responsive
-          </div>
-          <img className={styles.image2Icon} alt="" src="/image-2@2x.png" />
-        </div>
-        <div className={styles.todocomplete1}>
-          <div className={styles.updateFlow}>Make responsive on Figma</div>
-          <img className={styles.image2Icon} alt="" src="/image-2@2x.png" />
-        </div>
-        <div className={styles.todocomplete2}>
-          <img className={styles.image2Icon} alt="" src="/image-1@2x.png" />
-          <div className={styles.updateFlow}>Finish designs</div>
-        </div>
-        <div className={styles.divider1} />
         <div className={styles.header}>
           <div className={styles.titleNotes}>
             <div className={styles.toDo}>To Do</div>
@@ -49,6 +14,17 @@ const ToDoListDesign = () => {
             <div className={styles.div}>🌤️</div>
           </div>
         </div>
+        <div className={styles.divider} />
+        <div className={styles.todocomplete}>
+          <img className={styles.image1Icon} alt="" src="/image-1@2x.png" />
+          <div className={styles.finishDesigns}>Finish designs</div>
+        </div>
+        <div className={styles.todocomplete1}>
+          <img className={styles.image1Icon} alt="" src="/image-2@2x.png" />
+          <div className={styles.finishDesigns}>Make responsive on Figma</div>
+        </div>
+        <div className={styles.divider} />
+        <div className={styles.addToDo}>Add to do...</div>
       </div>
     </div>
   );
